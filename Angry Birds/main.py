ab89665dea32810ed2020025c95adbd7faaead94
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2022-11-06 12:19:33 +0530

    Add files via upload

---
+++ Angry Birds/main.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import math
+import pymunk as pm
+import time
+import pygame
+
+
+
+
+pygame.init()
+screen = pygame.display.set_mode((1200,600))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
